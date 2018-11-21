681184f1cad4fb3385f7cf8a0f8d7346194a76eb
HeinTouchableFR <aymericlhomme@orange.fr>
2018-11-21 11:20:12 +0100

    interface principale complete

--- src/vue/Fprincipale.java
+++ src/vue/Fprincipale.java
@@ -10,13 +10,18 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileSystemView;
+import javax.swing.text.Caret;
 
 import java.io.BufferedReader;
 import java.io.File;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JList;
 
 public class Fprincipale extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textFieldChemin;
 
 	/**
 	 * Launch the application.
@@ -39,27 +44,58 @@ public class Fprincipale extends JFrame {
 	 */
 	public Fprincipale() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 663, 420);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		textFieldChemin = new JTextField();
+		textFieldChemin.setBounds(21, 37, 252, 20);
+		contentPane.add(textFieldChemin);
+		textFieldChemin.setColumns(10);
+		
 		JButton btnChoisirRpertoire = new JButton("Choisir r\u00E9pertoire");
 		btnChoisirRpertoire.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+				JFileChooser chooser = new JFileChooser();
+			    chooser.setCurrentDirectory(new java.io.File("."));
+			    chooser.setDialogTitle("choosertitle");
+			    chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			    chooser.setAcceptAllFileFilterUsed(false);
 
-				int returnValue = jfc.showOpenDialog(null);
-				// int returnValue = jfc.showSaveDialog(null);
-
-				if (returnValue == JFileChooser.APPROVE_OPTION) {
-					File selectedFile = jfc.getSelectedFile();
-					System.out.println(selectedFile.getAbsolutePath());
-				}
+			    if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			      textFieldChemin.setText(chooser.getSelectedFile().getAbsolutePath());
+			    } else {
+			      System.out.println("No Selection ");
+			    }
 			}
 		});
-		btnChoisirRpertoire.setBounds(188, 36, 145, 23);
+		btnChoisirRpertoire.setBounds(283, 36, 145, 23);
 		contentPane.add(btnChoisirRpertoire);
+		
+		JButton btnLire = new JButton("Lire");
+		btnLire.setBounds(438, 36, 89, 23);
+		contentPane.add(btnLire);
+		
+		JList listFichiers = new JList();
+		listFichiers.setBounds(21, 103, 252, 267);
+		contentPane.add(listFichiers);
+		
+		JList listErreur = new JList();
+		listErreur.setBounds(418, 100, 207, 270);
+		contentPane.add(listErreur);
+		
+		JLabel lblErreurDeTransmission = new JLabel("Erreur de transmission");
+		lblErreurDeTransmission.setBounds(418, 75, 207, 14);
+		contentPane.add(lblErreurDeTransmission);
+		
+		JLabel lblLectureDeXx = new JLabel("Lecture de XX Fichier(s)");
+		lblLectureDeXx.setBounds(21, 78, 252, 14);
+		contentPane.add(lblLectureDeXx);
+		
+		JButton btnAnalyser = new JButton("Analyser");
+		btnAnalyser.setBounds(297, 187, 89, 23);
+		contentPane.add(btnAnalyser);
 	}
 }
